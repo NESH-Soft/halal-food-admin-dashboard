b006e1b4c1f2069734ab1700ed24847d61599135
Abdullah Naser <abdnaser.exe@gmail.com>
2020-12-14 14:11:46 +0600

    app const added

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
+const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
